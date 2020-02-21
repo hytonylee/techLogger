a30e39980f330a392ed4338c5cb944272f7852fe
Tony Lee <hy.tony.lee@gmail.com>
2020-02-20 18:11:55 -0800

    Hook up deleteLog at LogItem

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import Moment from 'react-moment';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { deleteLog } from '../../actions/logActions';
 
-const LogItem = ({ log }) => {
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+const LogItem = ({
+	log: { attention, message, id, tech, date },
+	deleteLog
+}) => {
+	const onDelete = () => {
+		deleteLog(id);
+
+		M.toast({ html: 'Log deleted!' });
+	};
+
 	return (
 		<li className='collection-item'>
 			<div>
 				<a
 					href='#edit-log-modal'
-					className={`modal-trigger ${
-						log.attention ? 'red-text' : 'blue-text'
-					}`}
+					className={`modal-trigger ${attention ? 'red-text' : 'blue-text'}`}
 				>
-					{log.message}
+					{message}
 				</a>
 				<br />
 				<span className='grey-text'>
-					<span className='black-text'>ID: #{log.id} </span>last updated by{' '}
-					<span className='black-text'>{log.tech}</span>
+					<span className='black-text'>ID: #{id} </span>last updated by{' '}
+					<span className='black-text'>{tech}</span>
 				</span>{' '}
-				on <Moment format='MMMM Do YYYY, h:mm:ss a'>{log.date}</Moment>
-				<a href='#1' className='secondary-content'>
+				on <Moment format='MMMM Do YYYY, h:mm:ss a'>{date}</Moment>
+				<a href='#1' className='secondary-content' onClick={onDelete}>
 					<i className='material-icons grey-text'>delete</i>
 				</a>
 			</div>
@@ -29,7 +40,8 @@ const LogItem = ({ log }) => {
 };
 
 LogItem.propTypes = {
-	log: PropTypes.object.isRequired
+	log: PropTypes.object.isRequired,
+	deleteLog: PropTypes.func.isRequired
 };
 
-export default LogItem;
+export default connect(null, { deleteLog })(LogItem);
